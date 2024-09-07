63355f88283de202c733a9030ed6209378f1fb32
Varun Arora <varunarora137@gmail.com>
2024-09-07 20:24:24 +0530

    modified day7_cw

--- day7_cw/backend/middlewares/urlValidator.js
+++ day7_cw/backend/middlewares/urlValidator.js
@@ -1,5 +1,15 @@
 import validator from "validator";
 
+const options = {
+  protocols: ["http", "https", "ftp"],
+  require_protocol: false,
+  require_host: true,
+  require_port: false,
+  require_valid_protocol: true,
+  allow_fragments: true,
+  allow_query_components: true,
+  disallow_auth: false,
+};
 export const urlValidator = (req, res, next) => {
   const { originalURL } = req.body;
   if (validator.isURL(originalURL)) {
